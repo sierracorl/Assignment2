3aa065f85d9513f34c54ad1f729f4083d752d91c
sierracorl <124177021+sierracorl@users.noreply.github.com>
2023-02-20 10:02:19 -0500

    Update Json.py

--- Json.py
+++ Json.py
@@ -4,50 +4,13 @@ import re
 
 app = Flask(__name__)
 
-# Define URL for Twitter favorites JSON file
+#URL for Twitter favorites - JSON file
 json_url = "https://foyzulhassan.github.io/files/favs.json"
 
-# Read and parse JSON file from URL
+# Read & parse JSON file from URL
 data_json = request.get(json_url).json()
  
-# Get tweets w/ time, ID & text
-@app.route('/tweets', methods=['GET'])
-def get_tweets():
-    tweets = []
-    for item in data_json:
-        tweet = {}
-        tweet['timeCreated'] = item['createdAt']
-        tweet['id'] = item['id_str']
-        tweet['text'] = item['txt']
-        tweets.append(tweet)
-    return jsonify(tweets)
-
-# Get external links by tweet ID
-@app.route('/link', methods=['GET'])
-def get_link():
-    link = {}
-    for item in data_json:
-        tweetLink = []
-        matches = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', item['full_text'])
-        for match in matches:
-            tweetLink.append(match)
-        link[item['id_str']] = tweetLink
-    return jsonify(link)
-
-# Get details of a tweet with ID
-@app.route('/tweets/<tweet_id>', methods=['GET'])
-def get_tweet(tweet_id):
-    for item in data_json:
-        if item['id_str'] == tweet_id:
-            tweet = {}
-            tweet['timeCreated'] = item['createdAt']
-            tweet['text'] = item['txt']
-            tweet['screenName'] = item['user']['screenName']
-            tweet['lang'] = item['lang']
-            return jsonify(tweet)
-    return "Tweet not found"
-
-# Get profile info from a user 
+ # Get profile info from user 
 @app.route('/users/<screenName>', methods=['GET'])
 def get_user(screenName):
     for screenName in data_json:
@@ -61,6 +24,44 @@ def get_user(screenName):
     else:
             return "User not found"
 
-# Run the Flask app
+#Get tweets w/ time, ID & text
+@app.route('/tweets', methods=['GET'])
+def get_tweets():
+    tweets = []
+    for item in data_json:
+        tweet = {}
+        tweet['timeCreated'] = item['createdAt']
+        tweet['id'] = item['id_str']
+        tweet['text'] = item['txt']
+        tweets.append(tweet)
+    return jsonify(tweets)
+
+#Get details of tweet w/ ID
+@app.route('/tweets/<tweet_id>', methods=['GET'])
+def get_tweet(tweet_id):
+    for item in data_json:
+        if item['id_str'] == tweet_id:
+            tweet = {}
+            tweet['timeCreated'] = item['createdAt']
+            tweet['text'] = item['txt']
+            tweet['screenName'] = item['user']['screenName']
+            tweet['lang'] = item['lang']
+            return jsonify(tweet)
+    return "Tweet not found"
+
+#Get external links by tweet ID
+@app.route('/link', methods=['GET'])
+def get_link():
+    link = {}
+    for item in data_json:
+        tweetLink = []
+        matches = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', item['all_text'])
+        for match in matches:
+            tweetLink.append(match)
+        link[item['id_str']] = tweetLink
+    return jsonify(link)
+
+
+#Run the Flask application
 if __name__ == '__main__':
     app.run(debug=True, host="127.0.0.1", port=5000)
